4e5829ccbfcaaa917c7fef40374b05dddd5c4c1e
Ansgar Lichter <lichteransgar@gmail.com>
2024-03-17 19:03:37 +0100

    fix: language determination not used

---
+++ ExportKindleClippingsToNotion/Parser/BooksParserFactory.cs
@@ -0,0 +1,11 @@
+﻿using ExportKindleClippingsToNotion.Import.Metadata;
+
+namespace ExportKindleClippingsToNotion.Parser;
+
+public class BooksParserFactory(IBookMetadataFetcher metadataFetcher) : IBooksParserFactory
+{
+    public IBooksParser Create(IClippingsParser clippingsParser)
+    {
+        return new BooksParser(metadataFetcher, clippingsParser);
+    }
+}
